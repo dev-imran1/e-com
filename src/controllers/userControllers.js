a395c6a3e0179551193e596a49f73f64bd44f890
dev-imran1 <freealltime247@gmail.com>
2024-09-29 00:39:29 +0600

    bycript

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,13 +1,21 @@
 import { User } from "../models/userSchema.js"
+import bcrypt from 'bcrypt';
 
-const createUser = async (req,res)=>{
-    
-    const {email} = req.body
-    const isFound = await User.findOne({email})
+
+const createUser = async (req, res) => {
+
+    const {displayName,email,password} = req.body;
+    const isFound = await User.findOne({ email })
     if (isFound) {
         res.send("email ache")
     }
-    res.send("okay create email")
+    const bpass = await bcrypt.hash(password, 10)
+
+    const user = await User.create({displayName,email,password:bpass})
+
+    console.log(user)
 }
 
-export {createUser}
+//29 minutes video 2 nodemailer install and setup, stmp.gmail.com
+
+export { createUser }
