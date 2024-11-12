6568e8fe18ca66af2bc358a95195a579f97cde7a
dev-Imran1 <freealltime247@gmail.com>
2024-11-12 16:02:33 +0600

    done

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,74 +3,98 @@ import { Product } from "../models/productModelSchema.js";
 import { cloudinaryUpload } from "../services/cloudinary.js";
 
 const createProduct = async (req, res) => {
-    try {
-    
-        const { title, category, subCategory, slug } = req.body;
-        const { thumbnail } = req.files
-        if ([title, slug, category, subCategory].some((field) => field === "")) {
-            return res.status(400).json({ error: "All fields are required" });
-        }
-
-        if (!thumbnail) {
-            return res.json("thumnail required")
-        }
-        let newSlug
-        if (!slug) {
-            newSlug = title.replace(" ", "-").toLowerCase() + "-" + Date.now()
-        } else {
-            const isSlugUniqe = await Product.findOne({ slug })
-            if (isSlugUniqe) {
-                return res.json("slug must be unique")
-            }
-            newSlug = slug.replace(" ", "-").toLowerCase() + "-" + Date.now();
-        }
-        const { path } = thumbnail[0]
-        const result = await cloudinaryUpload(path, slug, "product")
-
-        const product = new Product()
-        if (req.files?.gallery) {
-            let public_id
-
-            const {gallery } = req.files
-            const galleryImages = gallery.map((item)=>item)
-
-            for (let image of galleryImages) {
-                public_id = image.filename + Date.now() + '-' + Math.round(Math.random() * 1E9)
-                const uploadGalleryImage = await cloudinaryUpload(
-                    image.path,
-                    public_id,
-                    'product/gallery'
-                )
-                
-                product.gallery.push({
-                    imagePath: uploadGalleryImage.optimizeUrl,
-                    public_Id:public_id,
-                })
-            }
-        }       
-        product.title = title
-        product.category = category
-        product.subCategory = subCategory
-        product.slug = newSlug
-        product.thumbnail.imagePath = result.optimizeUrl
-        product.thumbnail.public_Id = result.uploadResult.public_id
-        await product.save()
-        console.log(product);
-        
-    } catch (error) {
-        console.log(error.message)
+  try {
+    const { title, category, subCategory, slug } = req.body;
+    const { thumbnail } = req.files;
+    if ([title, slug, category, subCategory].some((field) => field === "")) {
+      return res.status(400).json({ error: "All fields are required" });
     }
-}
+
+    if (!thumbnail) {
+      return res.json("thumnail required");
+    }
+    let newSlug;
+    if (!slug) {
+      newSlug = title.replace(" ", "-").toLowerCase() + "-" + Date.now();
+    } else {
+      const isSlugUniqe = await Product.findOne({ slug });
+      if (isSlugUniqe) {
+        return res.json("slug must be unique");
+      }
+      newSlug = slug.replace(" ", "-").toLowerCase() + "-" + Date.now();
+    }
+    const { path } = thumbnail[0];
+    const result = await cloudinaryUpload(path, slug, "product");
+
+    const product = new Product();
+    if (req.files?.gallery) {
+      let public_id;
+
+      const { gallery } = req.files;
+      const galleryImages = gallery.map((item) => item);
+
+      for (let image of galleryImages) {
+        public_id =
+          image.filename + Date.now() + "-" + Math.round(Math.random() * 1e9);
+        const uploadGalleryImage = await cloudinaryUpload(
+          image.path,
+          public_id,
+          "product/gallery"
+        );
+
+        product.gallery.push({
+          imagePath: uploadGalleryImage.optimizeUrl,
+          public_Id: public_id,
+        });
+      }
+    }
+    product.title = title;
+    product.category = category;
+    product.subCategory = subCategory;
+    product.slug = newSlug;
+    product.thumbnail.imagePath = result.optimizeUrl;
+    product.thumbnail.public_Id = result.uploadResult.public_id;
+    await product.save();
+    console.log(product);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 const deleteProduct = (req, res) => {
+  try {
+    const { id } = req.params;
+    Inventory.deleteMany({ product: id });
+    Inventory.findByIdAndDelete({ _id: id });
+    return res.send("product deleted");
+  } catch (error) {
+    console.log(error, "catch error in deleteProduct controller");
+  }
+};
 
-    try {
-        const { id } = req.params
-         Inventory.deleteMany({product:id})
-        res.send("product deleted")
-    } catch (error) {
-        
+const pagination = async (req, res) => {
+  try {
+    const { page, limit } = req.query;
+    let currentPage = 1;
+    if (page < 1) {
+      const baseUrl = limit || 2;
+      const skip = Number(currentPage - 1) * baseUrl;
+      const products = await Product.find().skip(skip).limit(baseUrl);
+      const totalProductCount = await Product.countDocuments();
+      const totalpage = await Math.ceil((totalProductCount/baseUrl))
+      console.log(totalpage)
+    } else {
+      const baseUrl = limit || 2;
+      const skip = Number(currentPage - 1) * baseUrl;
+      const products = await Product.find().skip(skip).limit(baseUrl);
+      const totalProductCount = await Product.countDocuments();
+      // console.log(totalProductCount);
+      const totalpage = await Math.ceil((totalProductCount/baseUrl))
+      console.log(totalpage,'aaa')
     }
-}
- 
-export { createProduct,deleteProduct }
+  } catch (error) {
+    console.log(error, "catch error in pagination controller");
+  }
+};
+
+export { createProduct, deleteProduct, pagination };
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -3,12 +3,13 @@ import { auth } from '../middlewares/authMiddleware.js';
 import { adminAuth } from '../middlewares/adminAuthMiddleware.js';
 import { allSubCategroy, subCategoryCreate } from '../controllers/subCategroyControllers.js';
 
-import { createProduct, deleteProduct } from '../controllers/productController.js';
+import { createProduct, deleteProduct, pagination } from '../controllers/productController.js';
 import { upload } from '../middlewares/multerMiddleware.js';
 
 const router = express.Router();
 
 router.route("/products/create").post(upload.fields([{ name: 'thumbnail', maxCount: 1 }, { name: 'gallery', maxCount: 4 }]), createProduct)
 router.route("/products/delete/:id").delete(deleteProduct)
+router.route("/products").get(pagination)
 
 export default router
